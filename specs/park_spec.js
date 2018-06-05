0335a6b01c5423a2b2397373773157761b00a77b
Kelsie Braidwood <kelsieiona@gmail.com>
2018-06-05 17:37:45 +0100

    test dinosaur has an array that starts empty

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -1,3 +1,21 @@
 const assert = require("assert");
 const Park = require("../park.js")
 const Dinosaur = require("../dinosaur.js")
+
+// 1. enclosure should start empty
+// 2. should be able to add dinosaur
+// 3. should be able to remove all dinosaurs of a particular type
+// 4. should get all the dinosaurs with an offspring count of more than 2
+
+describe("Park", function(){
+  let park;
+
+  beforeEach(function(){
+    park = new Park();
+  })
+
+  it("should have an enclosure that start empty", function(){
+    assert.strictEqual(park.enclosure.length, 0);
+  })
+
+});
